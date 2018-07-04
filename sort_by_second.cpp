82c79f56d2546c66e848326eb536c0877ff9482b
Subhadeep Deyashi <36545449+subhadeep-deyashi@users.noreply.github.com>
2018-07-04 21:41:05 +0530

    Delete sort_by_second.cpp

--- sort_by_second.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-bool sortbysec(const pair<long int,string> &a,
-              const pair<long int,string> &b)
-{
-    return (a.second < b.second);
-}
